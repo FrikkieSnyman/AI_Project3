e48846b7901dd31c3ace7c3ccac133477da7b899
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-05-26 14:59:39 +0200

    Started on trainer

--- Trainer.java
+++ Trainer.java
@@ -1,7 +1,11 @@
 public class Trainer{
-	NeuralNetwork nn;
-	
-	public Trainer(){
+	private NeuralNetwork nn;
+	private Double learningRate;
+	private Double momentum;
+	private Integer maxEpoch;
+	private Integer epoch = 0;
+		
+	public Trainer(Double learningRate, Double momentum, Integer maxEpoch){
 
 	}
 }
